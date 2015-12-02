a5a9172165ac13c94661b7b00ffae83f2102ad71
Phil Jauvin <philippejauvin@gmail.com>
2015-12-01 22:00:53 -0500

    Added redirect, cosmetic changes
    Added wildcard redirect to home page
    cosmetic changes to home page
    more API call documentation

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ express = require("express");
 
 var app = express();
 
+// Use html and css in public
+app.use(express.static("public"));
+
+// If connection is to anything other than an api route, redirect to home page
+app.get("*",function(req,res){
+  res.sendFile("/public/index.html");
+});
+
+// Listen on port 3000 and print that server is running
 app.listen(3000,function(req,res){
   console.log("Server running on port 3000");
 });
